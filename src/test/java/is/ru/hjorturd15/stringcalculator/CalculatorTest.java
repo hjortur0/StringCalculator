500e575d34fc11ee5bebdacca72dd11150186ad0
Hjortur Thor <hjorturd15@ru.is>
2017-10-13 18:28:09 +0000

    1. Test 1 - Test empty string

---
+++ src/test/java/is/ru/hjorturd15/stringcalculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package is.ru.hjorturd15.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, Calculator.add(""));
+    }
+    
+}
